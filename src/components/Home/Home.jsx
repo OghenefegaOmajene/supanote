2419620511d800a941bfbbf37d726fabc61956ab
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-03 18:23:56 +0100

    Home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <p>Your Notes and Tasks Are Messy...</p>
+        <p>Our AI Makes Them Work for You</p>
+
+        <p>Organize your thoughts, track your to-dos, and get smart suggestions in one unified workspace designed for start-up founders.</p>
+
+        <button>Get Started</button>
+    </div>
+  )
+}
+
+export default Home
