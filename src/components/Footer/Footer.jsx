83d588496397f9bbf225e94034114dee6a91f20c
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-04 23:25:04 +0100

    Footer design contd

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,12 +6,24 @@ const Footer = () => {
   return (
     <div className='footer'>
         <div className="footerBody">
-            <div className="footerLogo">
-                <img src={logo} alt="" />
-                <h2>SupaNote</h2>
+            <div className="footerLogoBox">
+                <div className="footerLogo">
+                    <img src={logo} alt="" />
+                    <h2>SupaNote</h2>
+                </div>
+                <p>AI Workspace for Notes and Tasks</p>
             </div>
-            <div className="footerLinks">
 
+            <div className="footerLinks">
+                <h2>Product</h2>
+                <p>Pro</p>
+                <p>Teams</p>
+            </div>
+
+            <div className="footerLinks">
+                <h2>Community</h2>
+                <p>X</p>
+                <p>GitHub</p>
             </div>
         </div>
         <div className="footerTxt">
