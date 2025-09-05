40206b7d115f30b543265bce6ed75df2f882b618
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-05 16:22:15 +0100

    Testimonial Card refactored

--- src/components/TestimonialCard/TestimonialCard.jsx
+++ src/components/TestimonialCard/TestimonialCard.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './TestimonialCard.css'
+import { IoIosStar } from "react-icons/io";
+import { BiSolidQuoteRight } from "react-icons/bi";
 
-const TestimonialCard = () => {
+const TestimonialCard = ({testimonial, name, founderImg}) => {
   return (
-    <div>TestimonialCard</div>
+    <div className='testimonialCard'>
+        <p className='testimonialText'>{testimonial}</p>
+
+        <div className="founderInfo">
+            <img src={founderImg} alt="" />
+
+            <div className="profile">
+                <p>{name}</p>
+
+                <div className="reviewStars">
+                    {[...Array(5)].map((_, index) => (
+                        <IoIosStar key={index} className="starIcon" />
+                    ))}
+                </div>
+            </div>  
+
+            <BiSolidQuoteRight/>
+            
+        </div>
+    </div>
   )
 }
 
--- src/sections/Testimonials/Testimonials.jsx
+++ src/sections/Testimonials/Testimonials.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Testimonials.css'
 import Capsule from '../../components/Capsule/Capsule'
 import { FaRegStar } from "react-icons/fa6";
+import TestimonialCard from '../../components/TestimonialCard/TestimonialCard';
+import founder1 from '../../assets/images/founder1.png'
 
 const Testimonials = () => {
   return (
@@ -9,11 +11,16 @@ const Testimonials = () => {
         <Capsule title="Testimonials" Icon={FaRegStar } />
         <div className='howitworksHead'>
             <p className='howitworksHead1'>What Founders Are Saying</p>
-            <p className='howitworksHead2'>StartNotesAi is helping startup leaders stay focused, ship faster, and feel in control.</p>
+            <p className='howitworksHead2'>SupaNote is helping startup leaders stay focused, ship faster, and feel in control.</p>
         </div>
 
         <div className="testimonialGrid">
-            <div className="div1"></div>
+            <TestimonialCard
+                className="div1"
+                testimonial="SupaNote is the smartest tool I’ve used all year. I just type in my messy thoughts, and it somehow turns them into a Monday plan. Magic."
+                name="Lauren K., Fintech Founder"
+                founderImg={founder1}
+            />
             <div className="div2"></div>
             <div className="div3"></div>
             <div className="div4"></div>
