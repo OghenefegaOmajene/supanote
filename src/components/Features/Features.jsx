871ee9bcd36ab2ca8ac098935b3001708bdcf40b
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-05 10:40:47 +0100

    Features section completed

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -10,37 +10,37 @@ const Features = () => {
   return (
     <div className='features'>
         <Capsule title="Features" Icon={BsStars } />
-        <div className='bentoTxtBox'>
-            <p className='bentoTxt'>You Focus on Building <br /> We Handle the Workflow.</p>
-            <p className='bentoTxt2'>Startup founders shouldn’t spend hours organizing. SupaNote does the heavy lifting, giving you clarity and momentum from day one.</p>
+        <div className='featuresHead'>
+            <p className='featuresHead1'>You Focus on Building <br /> We Handle the Workflow.</p>
+            <p className='featuresHead2'>Startup founders shouldn’t spend hours organizing. SupaNote does the heavy lifting, giving you clarity and momentum from day one.</p>
         </div>
 
-        <div className="featuresCards">
+        <div className="featureCards">
             <div className="featureCard">
                 <img src={feature1} alt="" />
 
                 <div className="featureTxt">
-                    <p>Organize Instantly, Not Manually</p>
+                    <h3>Organize Instantly, Not Manually</h3>
                     <p>No more hunting through scattered docs or sticky notes.</p>
                     <p>SupaNote uses AI to auto-tag and group your notes by theme, project, or meeting — keeping everything connected and searchable.</p>
                 </div>
             </div>
 
             <div className="featureCard">
-                <img src={feature2} alt="" />
-
                 <div className="featureTxt">
-                    <p>Get Smart Recommendations That Actually Help</p>
+                    <h3>Get Smart Recommendations That Actually Help</h3>
                     <p>You don’t need to figure out what to do next — we’ll tell you.</p>
                     <p>The AI analyzes your notes and tasks to suggest your top 3 daily priorities, follow-ups, and overdue tasks based on your goals.</p>
                 </div>
+
+                <img src={feature2} alt="" />
             </div>
 
             <div className="featureCard">
                 <img src={feature3} alt="" />
 
                 <div className="featureTxt">
-                    <p>Think Once. Take Action Forever.</p>
+                    <h3>Think Once. Take Action Forever.</h3>
                     <p>Make one note and get a whole plan.</p>
                     <p>Our note-to-action engine turns a single brain dump into structured tasks, timelines, and even follow-up reminders, saving you hours each week.</p>
                 </div>
