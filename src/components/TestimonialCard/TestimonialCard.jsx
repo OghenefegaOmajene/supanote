63202de4c7a3c1a82f8ee7958724bf596de7407a
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-05 16:02:03 +0100

    Testimonial Card & Pricing Card design fixes

---
+++ src/components/TestimonialCard/TestimonialCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TestimonialCard.css'
+
+const TestimonialCard = () => {
+  return (
+    <div>TestimonialCard</div>
+  )
+}
+
+export default TestimonialCard
--- src/sections/Testimonials/Testimonials.jsx
+++ src/sections/Testimonials/Testimonials.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import './Testimonials.css'
 import Capsule from '../../components/Capsule/Capsule'
+import { FaRegStar } from "react-icons/fa6";
 
 const Testimonials = () => {
   return (
     <div className='testimonials'>
-        <Capsule title="Pricing" Icon={LuTag } />
+        <Capsule title="Testimonials" Icon={FaRegStar } />
         <div className='howitworksHead'>
-            <p className='howitworksHead1'>Start Smart. Scale Fast.</p>
-            <p className='howitworksHead2'>Whether you're a solo founder or growing team, there's a plan to help you stay productive and focused.</p>
+            <p className='howitworksHead1'>What Founders Are Saying</p>
+            <p className='howitworksHead2'>StartNotesAi is helping startup leaders stay focused, ship faster, and feel in control.</p>
+        </div>
+
+        <div className="testimonialGrid">
+            <div className="div1"></div>
+            <div className="div2"></div>
+            <div className="div3"></div>
+            <div className="div4"></div>
+            <div className="div5"></div>
+            <div className="div6"></div>
         </div>
     </div>
   )
